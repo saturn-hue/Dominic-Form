c9f32627dd60b242a0bd7d0279f53a3a8f04f3b6
Saturday Emmanuel Okposio <59060588+saturn-hue@users.noreply.github.com>
2020-12-25 19:49:08 +0000

    Dominic Form

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/*$(function () {
+    $("#input").click(function () {
+        var password = $("#txtPassword").val();
+        var confirmPassword = $("#txtConfirmPassword").val();
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
+    });
+});*/
